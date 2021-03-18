9c52aba67b758bba388fe141884a017e5fb24f5e
DUDDLSBOT <78071781+DUDDLSBOT@users.noreply.github.com>
2021-03-18 17:23:25 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,78 @@
+const help = (prefix) => {
+	return `┌──「 *𝐃𝐔𝐃𝐔 𝐁𝐎𝐓* 」
+│
+├「 𝐒𝐓𝐀𝐓𝐔𝐒 𝐃𝐎 𝐏𝐑𝐎𝐏𝐑𝐈𝐄𝐓𝐀𝐑𝐈𝐎 」
+│
+├◪ *NAME: 𝐃𝐔𝐃𝐔*
+├◪ *WA.ME/5531986283462*
+├◪ *YT: *
+│
+├──「 *𝐁𝐎𝐓* 」
+│
+├◪ ${prefix}info
+├◪ ${prefix}proprietário
+├◪ ${prefix}doação
+├◪ ${prefix}lista de bloqueio
+│
+├──「 *𝐌𝐄𝐍𝐔 𝐒𝐈𝐌𝐏𝐋𝐄𝐒* 」
+│
+├◪ ${prefix}sticker
+├◪ ${prefix}sticker nobg
+├◪ ${prefix}toimg
+├◪ ${prefix}ttp
+├◪ ${prefix}tts
+├◪ ${prefix}nulis
+│
+├──「 *𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃* 」
+│
+├◪ ${prefix}tiktod
+├◪ ${prefix}ytmp3
+│
+├──「 *𝐌𝐄𝐌𝐄* 」
+│
+├◪ ${prefix}meme
+├◪ ${prefix}memeindo
+│
+├──「 *𝐆𝐑𝐔𝐏𝐎* 」
+│
+├◪ ${prefix}admin
+├◪ ${prefix}welcome [1/0]
+├◪ ${prefix}add
+├◪ ${prefix}kick
+├◪ ${prefix}promote
+├◪ ${prefix}demote
+├◪ ${prefix}tagall
+├◪ ${prefix}tagall2
+├◪ ${prefix}tagall3
+├◪ ${prefix}linkgrup
+├◪ ${prefix}leave
+├◪ ${prefix}simih [1/0]
+│
+├──「 *18+ 𝐌𝐄𝐍𝐔* 」
+│
+├◪ ${prefix}loli
+├◪ ${prefix}nsfwloli
+│
+├──「 *𝐎𝐔𝐓𝐑𝐎𝐒* 」
+│
+├◪ ${prefix}quotes
+├◪ ${prefix}ssweb
+├◪ ${prefix}simi
+├◪ ${prefix}ocr
+├◪ ${prefix}wait
+├◪ ${prefix}tiktokstalk
+├◪ ${prefix}hilih
+├◪ ${prefix}ytstalk
+│
+├──「 *𝐃𝐎𝐍𝐎* 」
+│
+├◪ ${prefix}setprefix
+├◪ ${prefix}bc
+├◪ ${prefix}clearall
+├◪ ${prefix}clone
+│
+└──「 *𝐃𝐔𝐃𝐔 𝐁𝐎𝐓* 」
+`
+}
+
+exports.help = help
